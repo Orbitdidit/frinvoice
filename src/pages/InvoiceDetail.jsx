e0cc0204ce25ebca8fb49ef87b03d9254f4b5b6f
Orbit Didit <orbitdidit@gmail.com>
2026-03-02 22:57:38 +0000

    File changes

--- src/pages/InvoiceDetail.jsx
+++ src/pages/InvoiceDetail.jsx
@@ -1,5 +1,4 @@
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { Invoice } from "@/entities/Invoice";
 import { User } from "@/entities/User";
